3b923c2ab791ddd6d04206738752cc4990106bc2
KyanTG <kyan.gijtenbeek@hva.nl>
2025-05-16 10:00:25 +0200

    added 404 to server

--- server.js
+++ server.js
@@ -35,3 +35,24 @@ app.set("port", process.env.PORT || 8000);
 app.listen(app.get("port"), function () {
   console.log(`Project draait via http://localhost:${app.get("port")}/\n\nSucces deze sprint. En maak mooie dingen! 🙂`);
 });
+<<<<<<< Updated upstream
+=======
+
+
+
+
+
+
+// app.get('/veronica/likes', async function (request, response) {
+
+//   const likedShows = await fetch('https://fdnd-agency.directus.app/items/mh_accounts/7?fields=id,name,liked_shows.mh_show_id.*.*.*')
+//   const likedShowsJSON = await likedShows.json()
+  
+// response.render('veronica-likes.liquid', {algemeen: likedShowsJSON.data} )  // hierdoor geef je de opgevraagde data mee in de naam algemeen
+// })
+
+// error page
+// app.use((req, res, next) => {
+//   res.status(404).render('notfound.liquid'); // custom error page
+// })
+>>>>>>> Stashed changes
