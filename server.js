3c009d5c16da5a8f3c4329052838e65d071a26ff
KyanTG <kyan.gijtenbeek@hva.nl>
2025-05-26 10:25:09 +0200

    dingen uit server veranderd, likes & post toegevoegd

--- server.js
+++ server.js
@@ -38,22 +38,32 @@ app.get("/", async function (request, response) {
   response.render("index.liquid");
 });
 
-// Stel het poortnummer in waar Express op moet gaan luisteren
-// Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
-app.set("port", process.env.PORT || 8000);
-
-// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
-app.listen(app.get("port"), function () {
-  console.log(`Project draait via http://localhost:${app.get("port")}/\n\nSucces deze sprint. En maak mooie dingen! 🙂`);
-});
 
 
 app.get("/veronica/likes", async function (request, response) {
   const likedShows = await fetch("https://fdnd-agency.directus.app/items/mh_accounts/7?fields=id,name,liked_shows.mh_show_id.*.*.*");
   const likedShowsJSON = await likedShows.json();
 
-  response.render("veronica-likes.liquid", { algemeen: likedShowsJSON.data }); // hierdoor geef je de opgevraagde data mee in de naam algemeen
+  response.render("veronica-likes.liquid", { algemeen: likedShowsJSON.data }); 
 });
+
+app.post('/veronica/like', async function (request, response) {
+
+  //console.log(request.body) 
+  const testConsole = await fetch('https://fdnd-agency.directus.app/items/mh_accounts_shows', {
+    method: 'POST',                       
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+    body: JSON.stringify({
+      mh_accounts_id: 7,                  
+      mh_show_id: request.body.showid     
+     }),
+  })
+    // console.log(testConsole)
+    response.redirect(303, '/veronica' )  
+})
+
 
 function sanitizeInput(input) {
   return input.replace(/</g, "&lt;").replace(/>/g, "&gt;");
@@ -148,3 +158,9 @@ app.use((req, res, next) => {
   res.status(404).render("404page.liquid"); // custom error page
 });
 
+
+app.set("port", process.env.PORT || 8000);
+
+app.listen(app.get("port"), function () {
+  console.log(`Project draait via http://localhost:${app.get("port")}`);
+});
